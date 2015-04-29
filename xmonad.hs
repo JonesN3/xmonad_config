50752fde90596fa1e1e604dbb6dcc60922e22ad1
Espen Adrian Jones <ep.jones92@gmail.com>
2015-04-29 19:12:05 +0200

    clean up on isle 3

--- xmonad.hs
+++ xmonad.hs
@@ -45,27 +45,38 @@ import qualified Data.Map as M
  
 -- Config {{{
 myTerminal      = "xterm"
+myStartScript = "~/.xmonad/.start"
+
+{- set to mod4Mask for super/windows key, 1 is alt -}
 modMask' :: KeyMask
 modMask' = mod1Mask
 
-modm' :: KeyMask
-modm' = mod1Mask
-
-
-
 -- myWorkspaces    = ["1: main","2:conf","3:web","4:xterm","5:music", "6:xterm", "7", "8", "9"]
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
--- Dzen/Conky
 
+{- Dzen/Conky status bar -}
 
-myXmonadBar = "~/usr/local/bin/dzen2 -x '0' -y '0' -h '18' -w '1420' -ta 'l' -fg '#FFFFFF' -bg '#6A7F8D'"
-myStatusBar = "/snacks/bin/conky -c /uio/hume/student-u56/espenaj/.xmonad/.conky_dzen | ~/usr/local/bin/dzen2 -x '1420' -w '500' -h '18' -ta 'r' -bg '#6A7F8D' -fg '#384E53' -y '0'"
+{- global definitions for status bar background and foreground -}
+myBarBg = "#6A7F8D"
+myBarFg = "#384E53"
+
+{- Local installations, change you your install location -}
+dzenLoc = "~/usr/local/bin/dzen2"
+conkyLoc = "~/snacks/bin/conky"
+conkyConfig = "~/.xmonad/.conky_dzen"
+
+{- commands to create the bars -}
+myXmonadBar = dzenLoc ++ " -x '0' -y '0' -h '18' -w '1420' -ta 'l' -fg '" ++ myBarBg ++ "' -bg '" ++ myBarBg ++ "'"
+myStatusBar = conkyLoc ++ " -c " ++ conkyConfig ++ " | " ++ dzenLoc ++ " -x '1420' -w '500' -h '18' -ta 'r' -bg'" ++ myBarBg ++ "' -fg '" ++ myBarFg ++ "' -y '0'"
+
 myBitmapsDir = "/uio/hume/student-u56/espenaj/.xmonad/dzen2"
+
 --}}}
 -- Main {{{
 main = do
     dzenLeftBar <- spawnPipe myXmonadBar
     dzenRightBar <- spawnPipe myStatusBar
+    spawn myStartScript
 
     xmonad $ defaultConfig
       { terminal            = myTerminal
@@ -78,57 +89,36 @@ main = do
       , normalBorderColor   = colorNormalBorder
       , focusedBorderColor  = colorFocusedBorder
       , borderWidth         = 2
-
       }
-
 --}}}
- 
  
 -- Hooks {{{
 -- ManageHook {{{
 manageHook' :: ManageHook
 manageHook' = (composeAll . concat $
     [ [resource     =? r            --> doIgnore            |   r   <- myIgnores] -- ignore desktop
-    , [className    =? c            --> doShift  "1:main"   |   c   <- myDev    ] -- move dev to main
-    , [className    =? c            --> doShift  "2:web"    |   c   <- myWebs   ] -- move webs to main
-    , [className    =? c            --> doShift  "3:vim"    |   c   <- myVim    ] -- move webs to main
-    , [className    =? c            --> doShift	 "4:chat"   |   c   <- myChat   ] -- move chat to chat
-    , [className    =? c            --> doShift  "5:music"  |   c   <- myMusic  ] -- move music to music
-    , [className    =? c            --> doShift  "6:gimp"   |   c   <- myGimp   ] -- move img to div
     , [className    =? c            --> doCenterFloat       |   c   <- myFloats ] -- float my floats
     , [name         =? n            --> doCenterFloat       |   n   <- myNames  ] -- float my names
     ]) 
  
     where
- 
         role      = stringProperty "WM_WINDOW_ROLE"
         name      = stringProperty "WM_NAME"
  
-        -- classnames
-        myFloats  = ["Smplayer","MPlayer","VirtualBox","Xmessage","XFontSel","Downloads","Nm-connection-editor"]
-        myWebs    = ["Firefox","Google-chrome","Chromium", "Chromium-browser"]
-        myMovie   = ["Boxee","Trine"]
-        myMusic	  = ["Rhythmbox","Spotify"]
-        myChat	  = ["Pidgin","Buddy List"]
-        myGimp	  = ["Gimp"]
-        myDev	  = ["xterm"]
-        myVim	  = ["Gvim"]
- 
-        -- resources
+        myFloats  = ["VirtualBox","Xmessage","XFontSel","Downloads","Nm-connection-editor"]
         myIgnores = ["desktop","desktop_window","notify-osd","stalonetray","trayer"]
- 
-        -- names
         myNames   = ["bashrun","Google Chrome Options","Chromium Options"]
- 
 -- }}}
-{- Enable gapping to alinge border perfect to top bar -}
+
+{- LayoutHook 
+ - Enable gapping to alinge border perfect to top bar 
+ - I like to have one workspace floating, just remove first line if you don't -}
 layoutHook'  =  
-                onWorkspaces ["9"] imLayout $
                 onWorkspace "4" simpleFloat $  
-                gaps [(U,20), (R,0)] $ Tall 1 (3/100) (1/2) ||| Full  
+                gaps [(U,19), (R,1), (L,1), (D,1)] $ Tall 1 (3/100) (1/2) ||| Full  
  
-{-Bar-}
-{- take 1 to print only first char of layout, take 0 to print none -}
+{- Bar
+ - take 1 to print only first char of layout, take 0 to print none -}
 myLogHook :: Handle -> X ()
 myLogHook h = dynamicLogWithPP $ defaultPP
     {
@@ -143,55 +133,23 @@ myLogHook h = dynamicLogWithPP $ defaultPP
         , ppOutput            =   hPutStrLn h
     }
  
--- Layout
-imLayout    = avoidStruts $ withIM (1%5) 
-    (And (ClassName "Pidgin") (Role "buddy_list")) Grid 
-
 --}}}
+
 -- Theme {{{
--- Color names are easier to remember:
-colorOrange         = "#FD971F"
-colorDarkGray       = "#1B1D1E"
-colorPink           = "#F92672"
-colorGreen          = "#A6E22E"
-colorBlue           = "#66D9EF"
-colorYellow         = "#E6DB74"
-colorWhite          = "#CCCCC6"
- 
 colorNormalBorder   = "#082B42"
 colorFocusedBorder  = "#146CA6"
  
- 
+{- not sure what these does -} 
 barFont  = "Caladea"
 barXFont = "inconsolata:size=12"
 xftFont = "xft: inconsolata-14"
 --}}}
  
--- Prompt Config {{{
-mXPConfig :: XPConfig
-mXPConfig =
-    defaultXPConfig { font                  = barFont
-                    , bgColor               = colorDarkGray
-                    , fgColor               = colorGreen
-                    , bgHLight              = colorGreen
-                    , fgHLight              = colorDarkGray
-                    , promptBorderWidth     = 0
-                    , height                = 14
-                    , historyFilter         = deleteConsecutive
-                    }
- 
--- Run or Raise Menu
-largeXPConfig :: XPConfig
-largeXPConfig = mXPConfig
-                { font = xftFont
-                , height = 22
-                }
 -- }}}
 -- Key mapping {{{
 {- modMask = alt, cant be changed in top of config -}
 keys' conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
-    [ ((modMask,                    xK_p        ), runOrRaisePrompt largeXPConfig)
-    , ((modMask,                    xK_Return   ), spawn $ XMonad.terminal conf)
+    [ ((modMask,                    xK_Return   ), spawn $ XMonad.terminal conf)
     , ((modMask,                    xK_F2       ), spawn "gmrun")
     , ((modMask .|. shiftMask,      xK_q        ), kill)
     , ((modMask .|. shiftMask,      xK_l        ), spawn "xscreensaver-command -lock")
@@ -232,7 +190,6 @@ keys' conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask .|. controlMask, xK_t), sendMessage $ ToggleGap U) -- toggle the top gap
     , ((modMask .|. controlMask, xK_w), sendMessage $ IncGap 1 U)  -- increment the top gap
     , ((modMask .|. controlMask, xK_q), sendMessage $ DecGap 1 U)  -- decrement the top gap
- 
  
     -- workspaces
     , ((modMask .|. controlMask,   xK_Right     ), nextWS)
